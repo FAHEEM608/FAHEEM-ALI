6acf007da2ac79ab78988c981c37ccc0cba6ba57
FAHEEM ALI <alibabapanhyar6@gmail.com>
2025-04-13 10:56:43 +0500

    Create Msg.js

---
+++ Msg.js
@@ -0,0 +1,15 @@
+      "description": "Make it true if you want bot read your all sms just now.",
+      "required": false,
+      "value": "false"
+    }
+
+},
+
+     "buildpacks": [
+        {
+            "url": "https://github.com/heroku/heroku-buildpack-nodejs.git"
+        }
+     ],
+  "stack": "heroku-24"
+}
+  
